80c94ca53f2bd9b0625b58658ae502f4a655454a
Hugo <ihfisk42@gmail.com>
2018-10-24 17:40:07 -0400

    Add files via upload

--- Depot.cpp
+++ Depot.cpp
@@ -23,3 +23,13 @@ void Depot::depotDechetsTraites(CamionBrun* _camion)
 {
 	totalDTCompostable += _camion->viderCamion;
 }
+
+std::ostream & operator<<(std::ostream & out, Depot const & depot)
+{
+	out << "-- DEPOT --" << std::endl
+		<< " totalDTRecyclable    : " << depot.getCamionBleu << std::endl
+		<< " totalDTNonRecyclable : " << depot.getCamionVert << std::endl
+		<< " totalDTCompostable   : " << depot.getCamionBrun << std::endl;
+
+	return out;
+}
--- Depot.h
+++ Depot.h
@@ -25,4 +25,6 @@ private:
 	int totalDTCompostable;
 };
 
+std::ostream & operator<<(std::ostream & out, Depot const & depot);
+
 #endif
